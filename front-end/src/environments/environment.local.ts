d68f1739aeb7a5aa30c1eaee73812ee37f753fc5
Mahmoud Mohammed <mahmoud_spiderman@yahoo.com>
2026-07-07 22:54:02 +0300

    dummy secrets

---
+++ front-end/src/environments/environment.local.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "key",
+    authDomain: "domain",
+    projectId: "id",
+    storageBucket: "storage",
+    messagingSenderId: "id",
+    appId: "appid",
+    measurementId: "measurmentid"
+  },
+  apiDomain: {
+    domain: 'http://localhost:3000' 
+  }
+};
